9ea3bcf5a9a46cb6d82fe08a8da3f7966a302a59
playdog-io <cqdxcs3@163.com>
2022-01-11 00:03:13 +0800

    0111

---
+++ ETH/dapp/dapp登链/codes/simpleDappTest/infoContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+contract InfoContract {
+    
+   string Name;
+   uint age;
+   
+   function setInfo(string _name, uint _age) public {
+       name = _name;
+       age = _age;
+   }
+   
+   function getInfo() public view returns (string, uint) {
+       return (name, age);
+   }   
+}
--- codes/test.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-// SPDX-License-Identifier: SimPL-2.0
-
-contract HelloWorld{
-    string name = "peilin";
-    function getName() public view returns(string memory){
-        return name;
-    }
-}
