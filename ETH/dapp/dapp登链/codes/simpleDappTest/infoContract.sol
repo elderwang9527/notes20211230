26317872c901c515ca4cca0585502de6b3121d4c
playdog-io <cqdxcs3@163.com>
2022-01-12 09:01:54 +0800

    0112

--- ETH/dapp/dapp登链/codes/simpleDappTest/infoContract.sol
+++ ETH/dapp/dapp登链/codes/simpleDappTest/infoContract.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.4.24;
 
 contract InfoContract {
-    
-   string Name;
-   uint age;
-   
-   function setInfo(string _name, uint _age) public {
-       name = _name;
-       age = _age;
-   }
-   
-   function getInfo() public view returns (string, uint) {
-       return (name, age);
-   }   
+    string Name;
+    uint256 age;
+
+    function setInfo(string _name, uint256 _age) public {
+        name = _name;
+        age = _age;
+    }
+
+    function getInfo() public view returns (string, uint256) {
+        return (name, age);
+    }
 }
